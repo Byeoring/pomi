d4a894ef007b60d148892aeafc2fc274bb0d75fb
Byeoring <87499591+Byeoring@users.noreply.github.com>
2023-10-07 15:35:19 +0900

    Update +layout.svelte

--- src/routes/(app)/lostark/+layout.svelte
+++ src/routes/(app)/lostark/+layout.svelte
@@ -1,5 +1,5 @@
 <div class="h-screen pt-2 pb-24 pl-2 pr-2 overflow-auto md:pt-0 md:pr-0 md:pl-0">
-    <div class="flex flex-col flex-wrap sm:flex-row ">
+    <div class="flex flex-wrap sm:flex-row ">
         <slot />
     </div>
 </div>
